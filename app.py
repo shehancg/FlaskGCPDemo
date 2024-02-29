fc2b095e53a5d8a0e29a0326d3cfab4a025c414d
Shehan Gunawardena <64646307+shehancg@users.noreply.github.com>
2024-02-29 19:58:42 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from dotenv import load_dotenv
+import os
+from flask_cors import CORS
+
+load_dotenv()
+
+app = Flask(__name__)
+CORS(app)
+
+app.config["DEBUG"] = os.environ.get('FLASK_DEBUG')
+
+@app.route('/')
+def hello_world():
+    return 'Hello Shehan'
+
+if __name__ == '__main__':
+    app.run()
